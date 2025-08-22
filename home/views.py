b9aafb541eba55ec8b489e808a8edd40617795d1
Serah Joyce Rasquinha <serahrasquinha@gmail.com>
2025-08-22 10:02:22 +0530

    35438

--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 def index(request):
     context = {
         'restaurant_name' : settings.RESTAURANT_NAME
+        'phone_number' : settings.RESTAURANT_PHONE_NUMBER
     }
     return render(request, 'home/index.html')
 
